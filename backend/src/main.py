35d855766c6ba738859ae52be89e255cf7f32ebf
Billones142 <stefanomerinoderui@gmail.com>
2026-06-17 12:24:37 -0300

    correccion de uvicorn

--- backend/src/main.py
+++ backend/src/main.py
@@ -87,4 +87,5 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
     # Ejecuta en puerto 8000 por defecto
-    uvicorn.run("main.py:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
